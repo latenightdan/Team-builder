710f6892fc09795ed2aa4e7f5004af3edae77deb
daniel natale <dnatale667@gmail.com>
2021-10-26 14:52:30 -0400

    Employee with test

---
+++ __tests__/Employee.test.js
@@ -0,0 +1,13 @@
+const Employee = require('../lib/Employee');
+
+test('creates an employee object', () =>{
+const employeee = new Employee('manager', 'butt');
+
+expect(employeee.name).toBe('manager');
+expect(employeee.name).toEqual(expect.any(String));
+expect(employeee.email).toBe('butt');
+expect(employeee.email).toEqual(expect.any(String));
+employeee.id += 1;
+expect(employeee.id).toEqual(1);
+console.log(employeee)
+});
